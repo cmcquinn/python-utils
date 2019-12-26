cd39a899d7a18abf915d3b3941fae4867679b0e8
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-12-25 17:03:47 -0700

    bintray.py: get commit hash and branch name from git, fix file upload

--- bintray.py
+++ bintray.py
@@ -24,8 +24,10 @@ def main():
     args = parser.parse_args()
 
     # build version string and append to url
-    commit = os.getenv('TRAVIS_COMMIT')
-    branch = os.getenv('TRAVIS_BRANCH')
+    result = subprocess.run('git rev-parse --short HEAD'.split(), stdout=subprocess.PIPE, encoding='UTF-8')
+    commit = result.stdout.strip() # get short commit hash from output of git rev-parse
+    result = subprocess.run('git rev-parse --abbrev-ref HEAD'.split(), stdout=subprocess.PIPE, encoding='UTF-8')
+    branch = result.stdout.strip() # get branch name from output of git rev-parse
     timefmt = '%Y%m%d%H%m'
     timestr = time.strftime(timefmt)
     version = '{time}-{branch}-{commit}'.format(
@@ -36,18 +38,24 @@ def main():
 
     # build url for upload
     bintray = 'https://api.bintray.com/content'
-    url = '{api}/{user}/{repo}/{package}/{version}/{name}?publish=1'.format(
-        api=bintray, user=args.user, repo=args.repo, package=args.package, version=version, name=name)
+    url = '{api}/{user}/{repo}/{name}'.format(
+        api=bintray, user=args.user, repo=args.repo, name=name)
 
     # get API key from environment variable
     key = os.getenv('BINTRAY_KEY')
 
-    # setup the file to be uploaded
-    file = {'file': open(args.file, 'rb')}
+    # build header
+    header = {
+        'X-Bintray-Package': args.package,
+        'X-Bintray-Version': version,
+        'X-Bintray-Publish': '1',
+        'X-Bintray-Override': '1'
+    }
 
     # upload to Bintray
     print('Uploading file {} to Bintray via POST request...'.format(name))
-    r = requests.put(url, files=file, auth=(args.user, key))
+    with open(args.file, 'rb') as fp:
+        r = requests.put(url, headers=header, data=fp, auth=(args.user, key))
 
     print('Response: {}'.format(r.text))
 
